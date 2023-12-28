d7c8f6ace9254d448f93f0da00d4f5ec507856e2
陶琪 <2455831076@qq.com>
2023-12-28 19:51:34 +0800

    测试

---
+++ index/src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <el-row>
+      <el-button @click="getList">默认按钮</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "HomeView",
+  methods:{
+    getList(){
+      console.log('11111')
+      this.axios.get('http://localhost:3312/user/getList',{
+        params:{
+          page:this.query.page,
+          size:this.query.size,
+          name:this.query.name,
+          phone:this.query.phone,
+          city:this.query.city,
+        }
+      }).then((resp)=>{
+        console.log(resp);
+        this.tableData = resp.data.content.list;
+        this.total = resp.data.content.total;
+      })
+    }
+  }
+};
+</script>
